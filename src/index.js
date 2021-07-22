d0a40aad3634e30d2a87d8b882ca99d3bb4235fb
IgooLapTop2 <igoo@mail.ru>
2021-07-22 12:33:01 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+//import Game from "./component/game";
+
+//ReactDOM.render(<Game />, document.getElementById("root"));
+
+import Table from "./component/table";
+
+ReactDOM.render(<Table />, document.getElementById("root"));
